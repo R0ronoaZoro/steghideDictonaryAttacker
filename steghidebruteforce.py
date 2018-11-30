90502c1f84bfe0ed52bfdbfc4d3ce35a0430ddb0
Chathura D Ranathunga <Chathurad92@gmail.com>
2018-11-30 12:18:32 +0530

    Update steghidebruteforce.py

--- steghidebruteforce.py
+++ steghidebruteforce.py
@@ -33,9 +33,8 @@ def steghideuse(filepath, image):
 					sys.stdout.write(ncolr+ "\n[*] Password is : " + colr + pwd + ncolr)
 					print("\n[*] Image is successfully extracted!")
 					quit()
-				else:
-					print("[*] Password Not found! Try different wordlist.")
 				line = fp.readline()
+			print()
 	except Exception as e:
 		print("[*] File not found!")
 
